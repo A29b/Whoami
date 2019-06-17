3ed92a2f3b0ed04b568fa3d85c160905f30a063b
klab <klab@mail.me>
2019-06-17 22:59:02 +0900

    [add] impl.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Whoami
+
+Whoami provide Identity which is based date and time.
+
+
+## License
+
+Whoami is licensed under the [MIT license](LICENSE.TXT).
---
+++ src/Whoami/Id.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Whoami
+{
+    public class Id
+    {
+        /// <summary>
+        /// Generate identity which is corresponded date and time.
+        /// </summary>
+        public static string Generate(DateTimeOffset datetime)
+        {
+            var ticks = datetime.ToUniversalTime().ToUnixTimeMilliseconds();
+
+            var bytes = BitConverter.GetBytes(ticks);
+            if (BitConverter.IsLittleEndian)
+                bytes = bytes.Reverse().ToArray();
+
+            // exclude paddigs of zero.
+            bytes = bytes.SkipWhile(x => x == byte.MinValue).ToArray();
+
+            // turning
+            var xor = bytes.Select(x => (byte)(x ^ byte.MaxValue)).ToArray();
+
+            var prefix = "ut";
+            var id = BitConverter.ToString(xor).Replace("-", string.Empty).ToLower();
+            return prefix + id;
+        }
+    }
+}
